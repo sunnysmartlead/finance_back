179545716b868ae65ef9b8b35c1ea46902259ec3
yangdh <dfjs2017>
2023-08-07 09:33:37 +0800

    提交代码

--- src/Finance.Application/Processes/BomEnterDtos/GetBomEntersInput.cs
+++ src/Finance.Application/Processes/BomEnterDtos/GetBomEntersInput.cs
@@ -1,7 +1,4 @@
 ﻿using Finance.Dto;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
 
 namespace Finance.Processes
 {
@@ -10,8 +7,5 @@ namespace Finance.Processes
     /// </summary>
     public class GetBomEntersInput: PagedInputDto
     {
-     
-        public int? Type { get; set; }
-        public List<FoundationLogsDto> ListFoundationLogs { get; set; }
     }
 }
