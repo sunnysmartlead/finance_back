51528f2c5b4280fa8b9a7e906cd9c5a1d181395d
yangdh <dfjs2017>
2023-08-10 17:00:16 +0800

    提交代码

--- aspnet-core/src/Finance.Core/Processes/FTWorkingHour.cs
+++ aspnet-core/src/Finance.Core/Processes/FTWorkingHour.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Finance.BaseLibrary
 {
-    [Table("FTWorkingHour")]
+    [Table("FWorkingHour")]
 	public class FTWorkingHour : FullAuditedEntity<long>
     {
 
