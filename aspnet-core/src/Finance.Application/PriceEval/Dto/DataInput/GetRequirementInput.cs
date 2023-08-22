93343ef850048a1a41ee986ff1978814f3caec4b
wszhuzy <wszhuzy@sunnyoptical.com>
2023-08-22 18:44:15 +0800

    [ADD] 对接接口

---
+++ aspnet-core/src/Finance.Application/PriceEval/Dto/DataInput/GetRequirementInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Finance.PriceEval.Dto.DataInput
+{
+    public class GetRequirementInput
+    {
+        /// <summary>
+        /// 审批流程主表ID
+        /// </summary>
+        public virtual long AuditFlowId { get; set; }
+
+        /// <summary>
+        /// 年份
+        /// </summary>
+        [Required]
+        public virtual int Year { get; set; }
+
+        /// <summary>
+        /// 年份上下
+        /// </summary>
+        public virtual YearType UpDown { get; set; }
+    }
+}
