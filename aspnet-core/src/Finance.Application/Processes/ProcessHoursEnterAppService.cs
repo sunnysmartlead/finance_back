3b95160ec0b90d6ad35f520c44d44896afc3807a
yangdh <dfjs2017>
2023-08-25 15:04:33 +0800

    提交

--- aspnet-core/src/Finance.Application/Processes/ProcessHoursEnterAppService.cs
+++ aspnet-core/src/Finance.Application/Processes/ProcessHoursEnterAppService.cs
@@ -363,7 +363,7 @@ namespace Finance.Processes
                     {
                         ProcessHoursEnteritem processHoursEnteritem =   new ProcessHoursEnteritem();
                         processHoursEnteritem.Year = year.Year;
-                        processHoursEnteritem.Id = foundationDevice;
+                        processHoursEnteritem.ProcessHoursEnterId = foundationDevice;
                         processHoursEnteritem.LaborHour= yearItem.LaborHour;
                         processHoursEnteritem.PersonnelNumber= yearItem.PersonnelNumber;
                         processHoursEnteritem.MachineHour= yearItem.MachineHour;
@@ -509,7 +509,7 @@ namespace Finance.Processes
                     {
                         ProcessHoursEnteritem processHoursEnteritem = new ProcessHoursEnteritem();
                         processHoursEnteritem.Year = year.Year;
-                        processHoursEnteritem.Id = entity.Id;
+                        processHoursEnteritem.ProcessHoursEnterId = entity.Id;
                         processHoursEnteritem.LaborHour = yearItem.LaborHour;
                         processHoursEnteritem.PersonnelNumber = yearItem.PersonnelNumber;
                         processHoursEnteritem.MachineHour = yearItem.MachineHour;
