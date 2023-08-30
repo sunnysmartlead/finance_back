4928aea28d357b0f76bcd00fc3c2a9616c56b890
琪 <77083642+guo-qq@users.noreply.github.com>
2023-08-30 18:07:41 +0800

    [FIX] 对接流程改动 (#53)
    * [ADD] NRE核价表
    
    * [FIX] Nre 核价表
    
    * [Fix] NRE 核价表
    
    * [FXI] 修改需求录入审核 的交互类
    
    * [ADD] 产品开发部审核
    
    * [FIX] 审核接口
    
    * [FIX] 解决swagger错误
    
    * [FIX] 修改注释
    
    * [FIX] Nre核价表
    
    * [FIX] Nre 核价表
    
    * [FIX] NrePricingMapper
    
    * [FIX] NRE核价表
    
    * [FIX] 问题
    
    * [FIX] Bom单价审核接口修改
    
    * [FIX] 修改参数同意有验证
    
    * [ADD]   线体数量 和共线分摊率
    
    * [FIX] 修改与刘工对接 时的问题
    
    * [FIX ] Nre核价表
    
    * [FIX] Nre 核价表
    
    * [FIX] Nre核价表
    
    * [FIX] 需求审核修改
    
    * [FIX] Nre
    
    * [FIX] 修改获取线体数量和共线分摊率的逻辑
    
    * [FIX] 修改线体数量和共线分摊率的逻辑
    
    * [FIx] 修改营销部审核录入接口
    
    * [FIX] 需改判断项目设计方案和方案是否一对一方法
    
    * [FIX] 将流代码取消注释
    
    * [FIX] 修改单价汇率的获取
    
    * [FIX] 修改审核方案表的名称 [FIX] 修改 环境录入 模板下载的方法
    
    * [FIX] 修改单价录入 获取汇率的方法
    
    * [FIX ]修改  营销部审核 方案表
    
    * [FIX] 修改日志表类型
    
    * [ADD] 毛利率方案的日志
    
    * [Add] 审核接口对接退回
    
    * [FIX] 修复核价表 生产设备费和差旅费 是有外键问题
    
    * [ADD] 损耗率
    
    * [ADD] 损耗率添加日志
    
    * [ADD] 新增 质量成本比例
    
    * [ADD] NRE 参数增加验证
    
    * [ADD] 添加参数验证
    
    * [FIX] 模具费
    
    * [FIX] 单价录入增加是否涉及选项 静态变量
    
    * [ADD] 获取电子结构单价返利金额方法
    
    * [FIX] 修改损耗率
    
    * [FIX] 损耗率改动修改
    
    * [FIX] 对接流程
    
    * [FIX] 修改 结构提交验证
    
    ---------
    
    Co-authored-by: 郭琪琪 <wsguoqq@sunnyoptical.com>

--- aspnet-core/src/Finance.Application/BaseLibrary/LogisticscostDtos/GetLogisticscostsInput.cs
+++ aspnet-core/src/Finance.Application/BaseLibrary/LogisticscostDtos/GetLogisticscostsInput.cs
@@ -9,5 +9,18 @@ namespace Finance.BaseLibrary
     {
         public long? AuditFlowId { get; set; }
         public long? SolutionId { get; set; }
+        /// <summary>
+        /// 节点实例Id
+        /// </summary>
+        public virtual long NodeInstanceId { get; set; }
+
+        /// <summary>
+        /// 审批意见
+        /// </summary>
+        public string Opinion { get; set; }
+        /// <summary>
+        /// 审批评论
+        /// </summary>
+        public string Comment { get; set; }
     }
 }
