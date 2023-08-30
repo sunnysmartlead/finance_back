f2c7110d0666b10183a58fcfc530bcaa40f1118d
wszhuzy <wszhuzy@sunnyoptical.com>
2023-08-30 17:45:56 +0800

    [REF] 公式工序接口对接工作流

--- aspnet-core/src/Finance.Application/BaseLibrary/LogisticscostDtos/GetLogisticscostsInput.cs
+++ aspnet-core/src/Finance.Application/BaseLibrary/LogisticscostDtos/GetLogisticscostsInput.cs
@@ -1,13 +1,27 @@
 ﻿using Finance.Dto;
+using Finance.WorkFlows.Dto;
+using System.ComponentModel.DataAnnotations;
 
 namespace Finance.BaseLibrary
 {
     /// <summary>
     /// 查询条件
     /// </summary>
-    public class GetLogisticscostsInput: PagedInputDto
+    public class GetLogisticscostsInput : PagedInputDto
     {
         public long? AuditFlowId { get; set; }
         public long? SolutionId { get; set; }
+    }
+
+    /// <summary>
+    /// ProcessHoursEnterAppService/CreateSubmitAsync方法的提交参数Dto
+    /// </summary>
+    public class ProcessHoursEnterCreateSubmitInput : SubmitNodeInput
+    {
+        /// <summary>
+        /// 流程Id
+        /// </summary>
+        [Required]
+        public virtual long AuditFlowId { get; set; }
     }
 }
