d2a83522e67e0f95cd6baba81be9456f2c2eb12f
yangdh <dfjs2017>
2023-09-01 18:02:45 +0800

    导出添加

---
+++ aspnet-core/src/Finance.Application/BaseLibrary/FoundationreliableDtos/FoundationreliableExport.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Application.Services.Dto;
+using MiniExcelLibs.Attributes;
+using System;
+
+namespace Finance.BaseLibrary
+{
+    /// <summary>
+    /// 列表
+    /// </summary>
+    public class FoundationreliableExport 
+    {
+        [ExcelColumnName("分类")]
+        public string Classification { get; set; }
+        [ExcelColumnName("名称")]
+        public string Name { get; set; }
+        [ExcelColumnName("价格")]
+        public double Price { get; set; }
+        [ExcelColumnName("单位")]
+        public string Unit { get; set; }
+    }
+}
