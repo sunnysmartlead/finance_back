b4e2515428ce7ea4be8175dc7a6fd7ed7a29087f
XYZ0908001 <99321012+XYZ0908001@users.noreply.github.com>
2023-09-10 15:14:27 +0800

    [ADD]跟线切线工时参数库 (#89)

---
+++ aspnet-core/src/Finance.Application/EngineeringDepartment/Dto/WorkingHoursV2Dto.cs
@@ -0,0 +1,89 @@
+﻿using Abp.Application.Services.Dto;
+using Finance.Dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.EngineeringDepartment.Dto
+{
+    public class WorkingHoursV2Dto
+    {
+        /// <summary>
+        /// 年份
+        /// </summary>
+
+        public int Year { get; set; }
+        /// <summary>
+        /// 人工工时
+        /// </summary>
+
+        public double LaborHour { get; set; }
+        /// <summary>
+        /// 机器工时
+        /// </summary>
+
+        public double MachineHour { get; set; }
+        /// <summary>
+        /// 人均跟线数量
+        /// </summary>
+
+        public double PerFollowUpQuantity { get; set; }
+    }
+    public class EditWorkingHoursV2Dto : EntityDto<long>
+    {
+
+        /// <summary>
+        /// 年份
+        /// </summary>
+
+        public int Year { get; set; }
+        /// <summary>
+        /// 人工工时
+        /// </summary>
+
+        public double LaborHour { get; set; }
+        /// <summary>
+        /// 机器工时
+        /// </summary>
+
+        public double MachineHour { get; set; }
+        /// <summary>
+        /// 人均跟线数量
+        /// </summary>
+
+        public double PerFollowUpQuantity { get; set; }
+
+    }
+
+
+
+    /// <summary>
+    /// 获取字典列表方法的参数输入
+    /// </summary>
+    public class GetWorkingHoursV2DtoListInput : PagedInputDto
+    {
+        /// <summary>
+        /// 年份
+        /// </summary>
+
+        public int Year { get; set; }
+        /// <summary>
+        /// 人工工时
+        /// </summary>
+
+        public double LaborHour { get; set; }
+        /// <summary>
+        /// 机器工时
+        /// </summary>
+
+        public double MachineHour { get; set; }
+        /// <summary>
+        /// 人均跟线数量
+        /// </summary>
+
+        public double PerFollowUpQuantity { get; set; }
+    }
+}
