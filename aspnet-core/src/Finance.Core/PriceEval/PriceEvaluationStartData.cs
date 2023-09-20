e7b1544496f0ee8d5562c2620e55798ecd118abf
Orb <39354295+WeiJunFenYou@users.noreply.github.com>
2023-09-20 13:45:07 +0800

    [ADD] 核价需求录入增加保存功能 (#108)
    * [ADD] 核价开始界面增加保存功能
    
    * [DEBUG] 解决报错
    
    * [REF] 变更逻辑
    
    ---------
    
    Co-authored-by: wszhuzy <wszhuzy@sunnyoptical.com>

---
+++ aspnet-core/src/Finance.Core/PriceEval/PriceEvaluationStartData.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Domain.Entities;
+using Castle.MicroKernel.SubSystems.Conversion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.PriceEval
+{
+    /// <summary>
+    /// 核价开始数据
+    /// </summary>
+    [Table("Pe_PriceEvaluationStartData")]
+    public class PriceEvaluationStartData : Entity<long>
+    {
+        /// <summary>
+        /// 审批流程主表Id
+        /// </summary>
+        [Required]
+        public virtual long AuditFlowId { get; set; }
+
+        /// <summary>
+        /// 录入的数据Json
+        /// </summary>
+        [Column(TypeName = "CLOB")]
+        public virtual string DataJson { get; set; }
+    }
+}
