462cca1cccc63095a163a335a8543753b951d98e
Zero656329 <865136678@qq.com>
2023-09-20 13:49:49 +0800

    Hwj (#107)
    * [ADD] 新增的报价看板接口
    
    * 成本信息表、经理审批、营销部审批
    
    * ADD 报价设备表新建
    
    * 【ADD】报价方案表新建，报价设备、动态单价、NRE表增加梯度、方案字段
    
    * 报价看板联调
    
    * 核价看板接口替换
    
    * 营销部审批后端代码
    
    * 总经理审批查看、财务归档数据，后端联调
    
    * 核心器件、对外报价
    
    * 修改字段product
    
    * [fix] 322、321、317、314问题修改
    
    * 326问题处理
    
    ---------
    
    Co-authored-by: Zero656329 <hwj656329@163.com>

--- aspnet-core/src/Finance.Application/MakeOffers/AnalyseBoard/DTo/AnalyseBoardSecondInputDto.cs
+++ aspnet-core/src/Finance.Application/MakeOffers/AnalyseBoard/DTo/AnalyseBoardSecondInputDto.cs
@@ -2,18 +2,13 @@
 using Finance.DemandApplyAudit;
 
 namespace Finance.MakeOffers.AnalyseBoard.DTo;
+
 /// <summary>
 /// 报价分析看板输入实体类  交互类
 /// </summary>
 public class AnalyseBoardSecondInputDto
 {
-   public long auditFlowId { get; set; }
-   /// <summary>
-   /// 毛利率方案id
-   ///
-   /// 
-   /// </summary>
-   public long GrossMarginId{ get; set; }
-   
-   public List<Solution> solutionTables{ get; set; }
+    public long auditFlowId { get; set; }
+    
+    public List<Solution> solutionTables { get; set; }
 }
