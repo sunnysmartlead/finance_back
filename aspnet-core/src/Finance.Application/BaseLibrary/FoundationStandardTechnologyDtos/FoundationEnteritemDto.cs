eb3ea86c05dc9edb958c531191dc9dc8e066087c
yangdh <dfjs2017>
2023-10-08 17:09:34 +0800

    标准工艺导出

---
+++ aspnet-core/src/Finance.Application/BaseLibrary/FoundationStandardTechnologyDtos/FoundationEnteritemDto.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Finance.BaseLibrary
+{
+    /// <summary>
+    /// 列表
+    /// </summary>
+    public class FoundationEnteritemDto : EntityDto<long>
+    {
+        public string LaborHour { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MachineHour { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NumberPersonnel { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Year { get; set; }
+    }
+}
