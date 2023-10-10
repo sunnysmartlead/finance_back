b087dd59ef1500aaab3a7c5e168857be86cb9c68
yangdh1 <62586628+yangdh1@users.noreply.github.com>
2023-10-10 16:39:46 +0800

    Yang0910 (#126)
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 数据库修改
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交
    
    * 提交
    
    * 提交代码
    
    * 提交点
    
    * t提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 枚举
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交
    
    * tij
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交
    
    * 提交
    
    * 提交代码
    
    * 提交
    
    * 提交
    
    * 提交
    
    * 提交
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交代码
    
    * 提交
    
    * 提交
    
    * 提交
    
    * 提交
    
    * 提交代码
    
    * 提交
    
    * 工时工序数据库调整
    
    * 数据库更新
    
    * 提交代码
    
    * 提交代码
    
    * 提提
    
    * 1
    
    * 提交
    
    * 合并代码
    
    * 合并代码
    
    * 增加年份id字段
    
    * 财务部物流优化
    
    * 优化标准工艺库
    
    * 提交代码
    
    * 导出优化
    
    * 时间修改
    
    * 新增导出接口
    
    * 导出添加
    
    * 合并代码
    
    * 提交参数
    
    * 修改名字不能重复
    
    * 查询按钮点击无效
    
    * 提交
    
    * 修复导入报错
    
    * 修改代码
    
    * 提交
    
    * 设备类型
    
    * 修改导入
    
    * 上传解析
    
    * 工序工时下拉列表
    
    * 提交
    
    * 根据编号湖区
    
    * 提交代码
    
    * 提交
    
    * 提交
    
    * 提交
    
    * 获取共线数量
    
    * 修改报错
    
    * 修改工时工序
    
    * 修改计算公式
    
    * 修改状态文字
    
    * 修改计算
    
    * 增加设备、工治具、软件、工时等总金额合计，之前设计中有的，只是颜色比较淡
    
    * 设备查询不到
    
    * 修改提示
    
    * 问题清单修改
    
    * 修改
    
    * 同一条工序下，不能维护多条数据
    
    * 工时工序导出
    
    * 下载模板添加下啦
    
    * 标准工艺导出
    
    * 修改导出
    
    * 增加字段
    
    ---------
    
    Co-authored-by: yangdh <dfjs2017>

---
+++ aspnet-core/src/Finance.Application/BaseLibrary/FoundationStandardTechnologyDtos/FoundationStandardSopInfoDto.cs
@@ -0,0 +1,34 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Finance.BaseLibrary
+{
+    /// <summary>
+    /// 列表
+    /// </summary>
+    public class FoundationStandardSopInfoDto : EntityDto<long>
+    {
+
+
+
+
+
+   
+
+
+        public List<FoundationEnteritemDto> Issues { get; set; } = new List<FoundationEnteritemDto>();
+
+        /// <summary>
+        /// 年
+        /// </summary>
+        public string Year { get; set; }
+        public decimal ModelCountYearId { get; set; }
+
+
+
+
+    }
+}
