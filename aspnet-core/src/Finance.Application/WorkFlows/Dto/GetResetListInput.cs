562d434b9be8c2d72985aed95e634a529683f419
Orb <39354295+WeiJunFenYou@users.noreply.github.com>
2023-11-02 16:10:26 +0800

    [ADD] 任务重置功能 (#199)
    * [ADD] 给核价任务增加重置功能
    
    * [ADD] 重置的查询方法

---
+++ aspnet-core/src/Finance.Application/WorkFlows/Dto/GetResetListInput.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.WorkFlows.Dto
+{
+    /// <summary>
+    /// 获取任务重置列表
+    /// </summary>
+    public class GetResetListInput : PagedResultRequestDto
+    {
+        /// <summary>
+        /// 工作流节点名称
+        /// </summary>
+        public virtual string NodeName { get; set; }
+
+        /// <summary>
+        /// 重置用户
+        /// </summary>
+        public virtual string ResetUser { get; set; }
+
+        /// <summary>
+        /// 目标用户
+        /// </summary>
+        public virtual string TargetUser { get; set; }
+    }
+}
