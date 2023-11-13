dc8efca561cf4d3ed536b4e9fd2ba381aceb434a
WeiJunFenYou <tdxh1357@163.com>
2023-11-13 16:18:16 +0800

    [REF] 客户目标价改为非必填

--- aspnet-core/src/Finance.Application/PriceEval/PriceEvaluationAppService.cs
+++ aspnet-core/src/Finance.Application/PriceEval/PriceEvaluationAppService.cs
@@ -1153,11 +1153,23 @@ namespace Finance.PriceEval
             }
             catch (Exception)
             {
-                throw new FriendlyException($"客户目标价录入的目标价格式不正确！");
-
+                //如果未填，或格式不正确，就设置为0
+                targetPrice = 0;
+                //throw new FriendlyException($"客户目标价录入的目标价格式不正确！");
             }
 
-            list.Add(new ProportionOfProductCostListDto { Name = "利润", Proportion = (targetPrice * customerTargetPrice.ExchangeRate) - sum });
+            decimal proportion;
+            if (targetPrice == 0 || customerTargetPrice.ExchangeRate is null)
+            {
+                proportion = 0;
+            }
+            else
+            {
+                proportion = (targetPrice * customerTargetPrice.ExchangeRate.Value) - sum;
+            }
+
+            list.Add(new ProportionOfProductCostListDto { Name = "利润", Proportion = proportion });
+
             return new ListResultDto<ProportionOfProductCostListDto>(list);
         }
 
