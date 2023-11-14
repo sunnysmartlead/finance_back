5a2194084addc6818aa2d80d36977b5327296588
WeiJunFenYou <tdxh1357@163.com>
2023-11-14 11:10:08 +0800

    [REF] 电子结构单价审核条件

--- aspnet-core/src/Finance.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/WorkFlowCreator.cs
+++ aspnet-core/src/Finance.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/WorkFlowCreator.cs
@@ -265,7 +265,7 @@ namespace Finance.EntityFrameworkCore.Seed.Host
                 {
                     Name="结构BOM单价审核",
                     FinanceDictionaryId = FinanceConsts.StructBomEvalSelect,
-                    Activation = $"{MainFlowId}_定制结构件_{MainFlowId}_结构BOM单价审核 && {MainFlowId}_结构BOM匹配修改_{MainFlowId}_结构BOM单价审核 && {MainFlowId}_核价看板_{MainFlowId}_结构BOM单价审核",
+                    Activation = $"({MainFlowId}_定制结构件_{MainFlowId}_结构BOM单价审核 && {MainFlowId}_结构BOM匹配修改_{MainFlowId}_结构BOM单价审核) || {MainFlowId}_核价看板_{MainFlowId}_结构BOM单价审核",
                     RoleId = structuralPriceAuditor.Id.ToString(),
                     ProcessIdentifier = "StructureUnitPriceEntryToExamine",
                 },
@@ -273,7 +273,7 @@ namespace Finance.EntityFrameworkCore.Seed.Host
                 {
                     Name="电子BOM单价审核",
                     FinanceDictionaryId = FinanceConsts.ElectronicBomEvalSelect,
-                    Activation = $"{MainFlowId}_电子BOM匹配修改_{MainFlowId}_电子BOM单价审核 && {MainFlowId}_核价看板_{MainFlowId}_电子BOM单价审核",
+                    Activation = $"{MainFlowId}_电子BOM匹配修改_{MainFlowId}_电子BOM单价审核 || {MainFlowId}_核价看板_{MainFlowId}_电子BOM单价审核",
                     RoleId = electronicsPriceAuditor.Id.ToString(),
                     ProcessIdentifier = "ElectronicUnitPriceEntryToExamine",
                 },
