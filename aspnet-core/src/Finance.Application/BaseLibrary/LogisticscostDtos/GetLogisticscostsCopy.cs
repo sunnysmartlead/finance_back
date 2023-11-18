d4394ac0a6e1df1a59a4157ddb103dbad680c7ec
yangdh <dfjs2017>
2023-11-18 21:36:28 +0800

    提交复制流程

---
+++ aspnet-core/src/Finance.Application/BaseLibrary/LogisticscostDtos/GetLogisticscostsCopy.cs
@@ -0,0 +1,16 @@
+﻿using Finance.Dto;
+using Finance.WorkFlows.Dto;
+using System.ComponentModel.DataAnnotations;
+
+namespace Finance.BaseLibrary
+{
+    /// <summary>
+    /// 复制条件
+    /// </summary>
+    public class GetLogisticscostsCopy
+    {
+        public long? AuditFlowId { get; set; }
+        public long? AuditFlowNewId { get; set; }
+      
+    }
+}
