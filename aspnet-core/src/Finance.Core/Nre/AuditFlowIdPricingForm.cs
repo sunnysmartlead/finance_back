8094ab6d8997d6194dc20f2120a81e0e2a78edc6
郭琪琪 <wsguoqq@sunnyoptical.com>
2023-11-21 11:12:19 +0800

    [ADD] NRE核价表快速核报价

---
+++ aspnet-core/src/Finance.Core/Nre/AuditFlowIdPricingForm.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Castle.MicroKernel.SubSystems.Conversion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Nre
+{
+    /// <summary>
+    /// Nre 核价表 带流程ID 实体类
+    /// </summary>
+    public class AuditFlowIdPricingForm : FullAuditedEntity<long>
+    {
+        /// <summary>
+        /// 审批流程主表ID
+        /// </summary>         
+        public long AuditFlowId { get; set; }
+        /// <summary>
+        /// 方案ID
+        /// </summary>       
+        public long SolutionId { get; set; }
+        /// <summary>
+        /// 将实体保存为JSON
+        /// </summary>
+        [Column(TypeName = "CLOB")]
+        public string JsonData { get; set; }
+    }
+}
