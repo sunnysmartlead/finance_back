093778651a8bfe6b7d5ce42ab83f289157e86f5e
郭琪琪 <wsguoqq@sunnyoptical.com>
2023-11-25 18:54:58 +0800

    [FIX] 报价单

--- aspnet-core/src/Finance.Application/MakeOffers/AnalyseBoard/Method/AnalysisBoardSecondMethod.cs
+++ aspnet-core/src/Finance.Application/MakeOffers/AnalyseBoard/Method/AnalysisBoardSecondMethod.cs
@@ -5659,7 +5659,7 @@ public class AnalysisBoardSecondMethod : AbpServiceBase, ISingletonDependency
 
         List<SolutionQuotationDto> solutionQuotations = await GeCatalogue(externalQuotationDto.AuditFlowId);
         //流程流转
-        if(solutionQuotations.Count== i+1)
+        if(solutionQuotations.Count== i+1&& externalQuotationDto.IsSubmit)
         {
             //嵌入工作流
             await _workflowInstanceAppService.SubmitNodeInterfece(new SubmitNodeInput
