82814c03025b1348479261137b30c0058baa762b
WeiJunFenYou <tdxh1357@163.com>
2023-11-25 21:27:20 +0800

    [REF] 邮件发送

--- aspnet-core/src/Finance.Application/WorkFlows/TradeComplianceEventHandler.cs
+++ aspnet-core/src/Finance.Application/WorkFlows/TradeComplianceEventHandler.cs
@@ -363,17 +363,17 @@ namespace Finance.WorkFlows
                             //发邮件给拥有这个流程的项目经理
                             #region 邮件发送
 
-                            //#if !DEBUG
+#if !DEBUG
                             SendEmail email = new SendEmail();
                             string loginIp = email.GetLoginAddr();
 
                             if (loginIp.Equals(FinanceConsts.AliServer_In_IP))
                             {
                                 var priceEvaluation = await _priceEvaluationRepository.FirstOrDefaultAsync(p => p.AuditFlowId == eventData.Entity.WorkFlowInstanceId);
-                                var role = await _roleRepository.FirstOrDefaultAsync(p =>
+                                var role = await _roleRepository.GetAllListAsync(p =>
                                 p.Name == StaticRoleNames.Host.FinanceTableAdmin || p.Name == StaticRoleNames.Host.EvalTableAdmin
                         || p.Name == StaticRoleNames.Host.Bjdgdgly);
-                                var userIds = await _userRoleRepository.GetAll().Where(p => p.RoleId == role.Id).Select(p => p.UserId).ToListAsync();
+                                var userIds = await _userRoleRepository.GetAll().Where(p => role.Select(p => p.Id).Contains(p.RoleId)).Select(p => p.UserId).ToListAsync();
 
                                 if (priceEvaluation != null)
                                 {
@@ -400,7 +400,7 @@ namespace Finance.WorkFlows
                                     }
                                 }
                             }
-                            //#endif
+#endif
                             #endregion
                         }
                         else
@@ -445,7 +445,7 @@ namespace Finance.WorkFlows
                             }
 
 #endif
-                            #endregion
+#endregion
                         }
 
                     }
