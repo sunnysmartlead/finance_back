2ca025e844bd676ec44034095b93b4dde746ae89
Orb <39354295+WeiJunFenYou@users.noreply.github.com>
2023-12-04 16:47:43 +0800

    [REF] 后台任务 (#424)
    * [ADD] 添加后台任务
    
    * [ADD] 给邮件设置后台任务
    
    ---------
    
    Co-authored-by: wszhuzy <wszhuzy@sunnyoptical.com>

---
+++ aspnet-core/src/Finance.Application/Job/PanelJsonClearJob.cs
@@ -0,0 +1,44 @@
+﻿using Abp.BackgroundJobs;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Finance.PriceEval;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Job
+{
+    public class PanelJsonClearJob : AsyncBackgroundJob<long>, ITransientDependency
+    {
+        private readonly IRepository<PanelJson, long> _panelJsonRepository;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public PanelJsonClearJob(IRepository<PanelJson, long> panelJsonRepository, IUnitOfWorkManager unitOfWorkManager)
+        {
+            _panelJsonRepository = panelJsonRepository;
+            _unitOfWorkManager = unitOfWorkManager;
+        }
+
+
+
+        /// <summary>
+        /// 清除核价表缓存
+        /// </summary>
+        /// <param name="auditFlowId"></param>
+        /// <returns></returns>
+        public async override Task ExecuteAsync(long auditFlowId)
+        {
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                using (_unitOfWorkManager.Current.SetTenantId(1))
+                {
+                    await _panelJsonRepository.DeleteAsync(p => p.AuditFlowId == auditFlowId);
+                }
+                uow.Complete();
+            }
+        }
+    }
+}
