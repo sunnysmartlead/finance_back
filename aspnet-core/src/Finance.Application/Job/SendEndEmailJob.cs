e0f294e5461d40ce9749432b044db503368b2b06
Orb <39354295+WeiJunFenYou@users.noreply.github.com>
2023-12-15 14:45:13 +0800

    [REF] 邮件的构造函数 (#465)
    Co-authored-by: wszhuzy <wszhuzy@sunnyoptical.com>

--- aspnet-core/src/Finance.Application/Job/SendEndEmailJob.cs
+++ aspnet-core/src/Finance.Application/Job/SendEndEmailJob.cs
@@ -2,6 +2,7 @@
 using Abp.BackgroundJobs;
 using Abp.Dependency;
 using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
 using Finance.Audit;
 using Finance.Authorization.Roles;
 using Finance.Authorization.Users;
@@ -24,57 +25,76 @@ namespace Finance.Job
         private readonly IRepository<PriceEvaluation, long> _priceEvaluationRepository;
         private readonly IRepository<Role, int> _roleRepository;
         private readonly IRepository<UserRole, long> _userRoleRepository;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public SendEndEmailJob(WorkflowInstanceAppService workflowInstanceAppService, IRepository<User, long> userRepository, IRepository<NoticeEmailInfo, long> noticeEmailInfoRepository, IRepository<PriceEvaluation, long> priceEvaluationRepository, IRepository<Role, int> roleRepository, IRepository<UserRole, long> userRoleRepository, IUnitOfWorkManager unitOfWorkManager)
+        {
+            _workflowInstanceAppService = workflowInstanceAppService;
+            _userRepository = userRepository;
+            _noticeEmailInfoRepository = noticeEmailInfoRepository;
+            _priceEvaluationRepository = priceEvaluationRepository;
+            _roleRepository = roleRepository;
+            _userRoleRepository = userRoleRepository;
+            _unitOfWorkManager = unitOfWorkManager;
+        }
 
         public async override Task ExecuteAsync(NodeInstance nodeInstance)
         {
-            #region 邮件发送
-
-            //#if !DEBUG
-            SendEmail email = new SendEmail();
-            string loginIp = email.GetLoginAddr();
-            var emailInfoList = await _noticeEmailInfoRepository.GetAllListAsync();
-
-
-            var priceEvaluation = await _priceEvaluationRepository.FirstOrDefaultAsync(p => p.AuditFlowId == nodeInstance.WorkFlowInstanceId);
-            var role = await _roleRepository.GetAllListAsync(p =>
-            p.Name == StaticRoleNames.Host.FinanceTableAdmin || p.Name == StaticRoleNames.Host.EvalTableAdmin
-    || p.Name == StaticRoleNames.Host.Bjdgdgly);
-            var userIds = await _userRoleRepository.GetAll().Where(p => role.Select(p => p.Id).Contains(p.RoleId)).Select(p => p.UserId).ToListAsync();
-
-            if (priceEvaluation != null)
+            using (var uow = _unitOfWorkManager.Begin())
             {
-                userIds.Add(priceEvaluation.ProjectManager);
-                if (priceEvaluation.CreatorUserId.HasValue
-                    && priceEvaluation.CreatorUserId != priceEvaluation.ProjectManager)
+                using (_unitOfWorkManager.Current.SetTenantId(1))
                 {
-                    userIds.Add(priceEvaluation.CreatorUserId.Value);
-                }
-            }
-            userIds = userIds.Distinct().ToList();
-            foreach (var userId in userIds)
-            {
-                var userInfo = await _userRepository.FirstOrDefaultAsync(p => p.Id == userId);
+                    #region 邮件发送
 
-                if (userInfo != null)
-                {
-                    string emailAddr = userInfo.EmailAddress;
-                    string loginAddr = "http://" + (loginIp.Equals(FinanceConsts.AliServer_In_IP) ? FinanceConsts.AliServer_Out_IP : loginIp) + ":8081/login";
-                    string emailBody = "核价报价提醒：您有新的工作流（" + nodeInstance.Name + "——流程号：" + nodeInstance.WorkFlowInstanceId + "）需要完成（" + "<a href=\"" + loginAddr + "\" >系统地址</a>" + "）";
+                    //#if !DEBUG
+                    SendEmail email = new SendEmail();
+                    string loginIp = email.GetLoginAddr();
+                    var emailInfoList = await _noticeEmailInfoRepository.GetAllListAsync();
 
-                    try
+
+                    var priceEvaluation = await _priceEvaluationRepository.FirstOrDefaultAsync(p => p.AuditFlowId == nodeInstance.WorkFlowInstanceId);
+                    var role = await _roleRepository.GetAllListAsync(p =>
+                    p.Name == StaticRoleNames.Host.FinanceTableAdmin || p.Name == StaticRoleNames.Host.EvalTableAdmin
+            || p.Name == StaticRoleNames.Host.Bjdgdgly);
+                    var userIds = await _userRoleRepository.GetAll().Where(p => role.Select(p => p.Id).Contains(p.RoleId)).Select(p => p.UserId).ToListAsync();
+
+                    if (priceEvaluation != null)
                     {
-                        if (!emailAddr.Contains("@qq.com"))
+                        userIds.Add(priceEvaluation.ProjectManager);
+                        if (priceEvaluation.CreatorUserId.HasValue
+                            && priceEvaluation.CreatorUserId != priceEvaluation.ProjectManager)
                         {
-                            await email.SendEmailToUser(loginIp.Equals(FinanceConsts.AliServer_In_IP), $"{nodeInstance.Name},流程号{nodeInstance.WorkFlowInstanceId}", emailBody, emailAddr, emailInfoList.Count == 0 ? null : emailInfoList.FirstOrDefault());
+                            userIds.Add(priceEvaluation.CreatorUserId.Value);
                         }
                     }
-                    catch (Exception)
+                    userIds = userIds.Distinct().ToList();
+                    foreach (var userId in userIds)
                     {
-                    }
-                }
-            }
+                        var userInfo = await _userRepository.FirstOrDefaultAsync(p => p.Id == userId);
 
-            #endregion
+                        if (userInfo != null)
+                        {
+                            string emailAddr = userInfo.EmailAddress;
+                            string loginAddr = "http://" + (loginIp.Equals(FinanceConsts.AliServer_In_IP) ? FinanceConsts.AliServer_Out_IP : loginIp) + ":8081/login";
+                            string emailBody = "核价报价提醒：您有新的工作流（" + nodeInstance.Name + "——流程号：" + nodeInstance.WorkFlowInstanceId + "）需要完成（" + "<a href=\"" + loginAddr + "\" >系统地址</a>" + "）";
+
+                            try
+                            {
+                                if (!emailAddr.Contains("@qq.com"))
+                                {
+                                    await email.SendEmailToUser(loginIp.Equals(FinanceConsts.AliServer_In_IP), $"{nodeInstance.Name},流程号{nodeInstance.WorkFlowInstanceId}", emailBody, emailAddr, emailInfoList.Count == 0 ? null : emailInfoList.FirstOrDefault());
+                                }
+                            }
+                            catch (Exception)
+                            {
+                            }
+                        }
+                    }
+
+                    #endregion
+                }
+                uow.Complete();
+            }
         }
     }
 }
