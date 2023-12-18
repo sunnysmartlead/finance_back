cb1697dd08e15c29a84ffe258217a73f95ff2579
wszhuzy <wszhuzy@sunnyoptical.com>
2023-12-18 18:20:08 +0800

    [ADD] 合并核价需求录入和产品开发部信息湖获取接口

---
+++ aspnet-core/src/Finance.Application/PriceEval/Dto/PePdDto.cs
@@ -0,0 +1,22 @@
+﻿using Finance.ProductDevelopment.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.PriceEval.Dto
+{
+    public class PePdDto
+    {
+        /// <summary>
+        /// 核价需求录入数据
+        /// </summary>
+        public PriceEvaluationStartInputResult PriceEvaluationStartInputResult { get; set; }
+
+        /// <summary>
+        /// 产品开发部录入信息
+        /// </summary>
+        public PostProductDevelopmentInputDto PostProductDevelopmentInputDto { get; set; }
+    }
+}
