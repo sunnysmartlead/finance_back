c595109c93ca93a601fba111e064b0c22efe48c7
yangdh <dfjs2017>
2024-01-15 11:28:28 +0800

    增加注释

--- aspnet-core/src/Finance.Core/Processes/FTWorkingHour.cs
+++ aspnet-core/src/Finance.Core/Processes/FTWorkingHour.cs
@@ -5,6 +5,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Finance.BaseLibrary
 {
+
+    /// <summary>
+    /// 标准工艺库--工时库
+    /// </summary>
     [Table("FTSWorkingHour")]
 	public class FTWorkingHour : FullAuditedEntity<long>
     {
@@ -29,21 +33,21 @@ namespace Finance.BaseLibrary
 		public string LaborHour { get; set; }
 
 		/// <summary>
-		/// 
+		/// 工时
 		/// </summary>
 		[Column("machine_hour")]
 		[StringLength(510, ErrorMessage = "MachineHour长度不能超出510字符")]
 		public string MachineHour { get; set; }
 
 		/// <summary>
-		/// 
+		/// 人员数量
 		/// </summary>
 		[Column("number_personnel")]
 		[StringLength(510, ErrorMessage = "NumberPersonnel长度不能超出510字符")]
 		public string NumberPersonnel { get; set; }
 
 		/// <summary>
-		/// 
+		/// 年份
 		/// </summary>
 		[Column("year")]
 		[StringLength(510, ErrorMessage = "Year长度不能超出510字符")]
