4b675b817fa6f86f48eeeb95eda73222542c170e
Orb <39354295+WeiJunFenYou@users.noreply.github.com>
2024-01-17 11:16:19 +0800

    Zzy 24 01 16 (#554)
    * [ADD] 时间更新增加条件约束
    
    * [REF] 取消项目经理可见核价需求录入
    
    * [REF] 流程结束时的逻辑
    
    * [REF] MOQ分摊成本计算
    
    * [ADD] 记录实体
    
    * [REF] 解决核价需求录入查不到的问题
    
    ---------
    
    Co-authored-by: wszhuzy <wszhuzy@sunnyoptical.com>

---
+++ aspnet-core/src/Finance.Core/WorkFlows/NodeTime.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.WorkFlows
+{
+    public class NodeTime : Entity<long>
+    {
+        /// <summary>
+        /// 节点Id
+        /// </summary>
+        public virtual long NodeInstance { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public virtual DateTime StartTime { get; set; }
+
+        public virtual DateTime UpdateTime { get; set; }
+    }
+}
