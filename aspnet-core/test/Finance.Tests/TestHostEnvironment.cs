91964b856b71f8b243bf56c0c152e740e59b7da5
Orb <39354295+WeiJunFenYou@users.noreply.github.com>
2024-02-22 18:56:51 +0800

    [REF] 解决测试用例中依赖注入注册失败的问题 (#598)
    * [REF] 修改函数名称
    
    * [REF] 更改测试用例的调用和连接字符串
    
    * [REF] 解决测试用例的依赖注入失败问题
    
    * [DEL] 无用的引用
    
    ---------
    
    Co-authored-by: wszhuzy <wszhuzy@sunnyoptical.com>

---
+++ aspnet-core/test/Finance.Tests/TestHostEnvironment.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Tests
+{
+    public class TestHostEnvironment : IWebHostEnvironment
+    {
+        public string EnvironmentName { get { return Environments.Development; } set { EnvironmentName = value; } }
+        public string ContentRootPath { get { return Directory.GetCurrentDirectory(); } set { ContentRootPath = value; } }
+        
+        public string WebRootPath { get { return Path.Combine(ContentRootPath, "wwwroot"); } set { WebRootPath = value; } }
+        
+        public IFileProvider WebRootFileProvider { get; set; } // 或者你可以提供一个实际的文件提供者  
+
+        public string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
