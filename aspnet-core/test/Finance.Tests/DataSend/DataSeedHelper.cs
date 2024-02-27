f01a993309f6c9ae378b1181cc9eafab6c6f17b7
wszhuzy <wszhuzy@sunnyoptical.com>
2024-02-27 17:49:23 +0800

    [REF] 1、恢复乱序执行 2、解决核价需求录入依赖国家库的问题 3、测试用例项目增加国家库数据种子

---
+++ aspnet-core/test/Finance.Tests/DataSend/DataSeedHelper.cs
@@ -0,0 +1,50 @@
+﻿using Abp.Dependency;
+using Abp.Domain.Uow;
+using Abp.MultiTenancy;
+using Finance.EntityFrameworkCore.Seed.Host;
+using Finance.EntityFrameworkCore.Seed.Tenants;
+using Finance.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Abp.EntityFrameworkCore.Uow;
+
+namespace Finance.Tests.DataSend
+{
+    internal class DataSeedHelper
+    {
+        public static void SeedHostDb(IIocResolver iocResolver)
+        {
+            WithDbContext<FinanceDbContext>(iocResolver, SeedHostDb);
+        }
+
+        public static void SeedHostDb(FinanceDbContext context, IUnitOfWorkManager unitOfWorkManager)
+        {
+            context.SuppressAutoSetTenantId = true;
+
+            // Host seed
+            new DataInitialHostDbBuilder(context, unitOfWorkManager).Create();
+        }
+
+        private static void WithDbContext<TDbContext>(IIocResolver iocResolver, Action<TDbContext, IUnitOfWorkManager> contextAction)
+            where TDbContext : DbContext
+        {
+            using (var uowManager = iocResolver.ResolveAsDisposable<IUnitOfWorkManager>())
+            {
+                //uowManager.Object
+                using (var uow = uowManager.Object.Begin(TransactionScopeOption.Suppress))
+                {
+                    var context = uowManager.Object.Current.GetDbContext<TDbContext>(MultiTenancySides.Host);
+
+                    contextAction(context, uowManager.Object);
+
+                    uow.Complete();
+                }
+            }
+        }
+    }
+}
---
+++ aspnet-core/test/Finance.Tests/DataSend/DefaultCountryLibraryCreator.cs
@@ -0,0 +1,50 @@
+﻿using Finance.EntityFrameworkCore;
+using Finance.Infrastructure;
+using Finance.TradeCompliance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Tests.DataSend
+{
+    internal class DefaultCountryLibraryCreator
+    {
+        private readonly FinanceDbContext _context;
+        public DefaultCountryLibraryCreator(FinanceDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            CreateCountryLibrary();
+        }
+
+        private void CreateCountryLibrary()
+        {
+            var countryLibraryList = new List<CountryLibrary>
+            {
+                new CountryLibrary { Id = 1, NationalType = "2", Country = "其他国家" },
+                new CountryLibrary { Id = 2, NationalType = "1", Country = "伊朗" },
+                new CountryLibrary { Id = 3, NationalType = "1", Country = "朝鲜" },
+                new CountryLibrary { Id = 4, NationalType = "1", Country = "古巴" },
+                new CountryLibrary { Id = 5, NationalType = "1", Country = "叙利亚" },
+            };
+            var noDb = countryLibraryList.Where(p => !_context.CountryLibrary.Contains(p));
+            if (noDb.Any())
+            {
+                _context.CountryLibrary.AddRange(noDb);
+                _context.SaveChanges();
+            }
+            var isDeleted = _context.CountryLibrary.Where(p => countryLibraryList.Contains(p) && p.IsDeleted).ToList();
+            if (isDeleted.Any())
+            {
+                isDeleted.ForEach(p => p.IsDeleted = false);
+                _context.CountryLibrary.UpdateRange(isDeleted);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
